1a78b6fd562c212bbb174c9530712fc56a61be30
William <48343678+WiIIiamTang@users.noreply.github.com>
2021-12-09 03:31:22 -0500

    add delimiter fix

--- scripts/clean_tweets.py
+++ scripts/clean_tweets.py
@@ -5,10 +5,18 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', required=True, help='input tweets tsv file')
     parser.add_argument('-o', required=False, default='test_tweets_cleaned.tsv', help='output file')
+    parser.add_argument('-d', required=False, default='\t', help='the file delimiter')
     return parser.parse_args()
 
 def main(**kwargs):
-    df = pd.read_csv(kwargs.get('i'), delimiter='\t')
+    sep = kwargs.get('delimiter')
+    df = pd.read_csv(kwargs.get('i'), delimiter=sep)
+    print(df)
+    df = df[df['keep'].astype(int)==1]
+
+    if len(df) != 1000:
+        print(f'Dataset does not have 1000 rows, got input of {len(df)} rows')
+        return
 
     # remove links, lowercase all tokens, remove punctuation and emojis
     df['text'] = (df['text']
@@ -20,9 +28,9 @@ def main(**kwargs):
 
     df['topics'] = df['topics'].apply(lambda s: str(s).strip())
 
-    df.to_csv(kwargs.get('o'), sep='\t', index=False)
+    df.to_csv(kwargs.get('o'), sep=sep, index=False)
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(i=args.i, o=args.o)
+    main(i=args.i, o=args.o, delimiter=args.d)
