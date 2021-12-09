ec3e9038b754fcda753d3344f6f98e534534aab4
William <48343678+WiIIiamTang@users.noreply.github.com>
2021-12-08 20:15:37 -0500

    add some scripts to view tweet data

---
+++ src/tweet_statistics.py
@@ -0,0 +1,40 @@
+import argparse
+import json
+import pandas as pd
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', required=True, help='input tweets tsv file')
+    parser.add_argument('-o', required=False, help='output file')
+    return parser.parse_args()
+
+def count_rows_by_key(df: pd.DataFrame, k: str, col1: str, col2: str) -> dict:
+    return json.loads(df[df[col1]==k][col2].value_counts().to_json())
+
+def main(**kwargs):
+    df = pd.read_csv(kwargs.get('i'), delimiter='\t')
+
+    results = {'overall': {}, 'by_topic': {}, 'by_sentiment': {}}
+    
+    results['overall']['topics'] = json.loads(df['topics'].value_counts().to_json())
+    results['overall']['sentiments'] = json.loads(df['sentiments'].value_counts().to_json())
+
+    results['by_topic'] = { 
+        k:count_rows_by_key(df, k, 'topics', 'sentiments')
+        for k in results['overall']['topics'].keys() 
+    }
+    results['by_sentiment'] = { 
+        k:count_rows_by_key(df, k, 'sentiments', 'topics')
+        for k in results['overall']['sentiments'].keys() 
+    }
+
+    if kwargs.get('o'):
+        with open(kwargs.get('o'), 'w') as f:
+            json.dump(results, f, indent=4)
+    
+    print(json.dumps(results, indent=4))
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(i=args.i, o=args.o)
